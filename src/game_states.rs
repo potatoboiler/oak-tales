c8b4c59f43d7aa7e1b48e5d54fbc94dabf895b7d
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-06-29 06:07:04 -0700

    Skeletons

--- src/game_states.rs
+++ src/game_states.rs
@@ -1,1 +1,9 @@
+use bevy::prelude::States;
 
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum GameState {
+    #[default]
+    Splash,
+    Menu,
+    Game,
+}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,38 @@ References:
 https://www.youtube.com/watch?v=qbeu0Mw1HLY
 https://github.com/mwbryant/rpg-bevy-tutorial/ refer to branches for specific tutorials. buttons are at tutorial 6
 */
-use bevy::prelude::*;
-use bevy_ecs_ldtk::prelude::*;
+//! This example illustrates the various features of Bevy UI.
 
-// mod game_states;
-fn main() {}
+pub mod game_states;
+
+use bevy::{
+    prelude::{default, App, AssetServer, Camera2dBundle, Commands, Res},
+    sprite::SpriteBundle,
+    DefaultPlugins,
+};
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
+        // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
+        .add_system(ui_example_system)
+        .add_startup_system(start_game)
+        .run();
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
+}
+
+fn start_game(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("img/Untitled.png"),
+        ..default()
+    });
+}
